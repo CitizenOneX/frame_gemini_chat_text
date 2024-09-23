eee2f0711cbfc77bf86841a3d130894e00fdbfe8
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-09-23 22:04:43 +1000

    added google_generative_ai package, shared_preferences for managing the user API_KEY, and sample code for requesting text generation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simple_frame_app/simple_frame_app.dart';
 
 void main() => runApp(const MainApp());
@@ -16,8 +18,11 @@ class MainApp extends StatefulWidget {
 
 /// SimpleFrameAppState mixin helps to manage the lifecycle of the Frame connection outside of this file
 class MainAppState extends State<MainApp> with SimpleFrameAppState {
+  String _apiKey = '';
+  final TextEditingController _textFieldController = TextEditingController();
+
   MainAppState() {
-    Logger.root.level = Level.INFO;
+    Logger.root.level = Level.FINE;
     Logger.root.onRecord.listen((record) {
       debugPrint(
           '${record.level.name}: [${record.loggerName}] ${record.time}: ${record.message}');
@@ -25,15 +30,45 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _loadApiKey();
+  }
+
+  Future<void> _loadApiKey() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _apiKey = prefs.getString('api_key') ?? '';
+      _textFieldController.text = _apiKey;
+    });
+  }
+
+  Future<void> _saveApiKey() async {
+    _apiKey = _textFieldController.text;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('api_key', _apiKey);
+  }
+
+
+  @override
   Future<void> run() async {
     currentState = ApplicationState.running;
     if (mounted) setState(() {});
 
     try {
-      // TODO do something
+      final model = GenerativeModel(
+        model: 'gemini-1.5-flash',
+        apiKey: _apiKey,
+      );
+
+      const prompt = 'Write a story about a magic backpack in Chinese.';
+
+      final response = await model.generateContent([Content.text(prompt)]);
+      print(response.text);
 
       currentState = ApplicationState.ready;
       if (mounted) setState(() {});
+
     } catch (e) {
       _log.fine('Error executing application logic: $e');
       currentState = ApplicationState.ready;
@@ -52,19 +87,25 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Simple Frame App Template',
+        title: 'Frame Gemini Chat',
         theme: ThemeData.dark(),
         home: Scaffold(
           appBar: AppBar(
-              title: const Text('Simple Frame App Template'),
+              title: const Text('Frame Gemini Chat'),
               actions: [getBatteryWidget()]),
-          body: const Padding(
-            padding: EdgeInsets.all(8.0),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Spacer(),
+                Row(
+                  children: [
+                    Expanded(child: TextField(controller: _textFieldController, obscureText: true, obscuringCharacter: '*', decoration: const InputDecoration(hintText: 'Enter Gemini api_key'),)),
+                    ElevatedButton(onPressed: _saveApiKey, child: const Text('Save'))
+                  ],
+                ),
+                ElevatedButton(onPressed: run, child: const Text('Run')),
               ],
             ),
           ),
